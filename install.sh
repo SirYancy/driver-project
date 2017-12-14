76e0f29cd8bab79c6d31301bb6d245e368fe3614
Student Account <student@csel-x22-umh.cselabs.umn.edu>
2017-12-14 09:35:47 -0600

    merging

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ printf "Installing Driver...\n"
 cd Buffer
 make
 python unload.py
-python load.py scull_size=10
+python load.py scull_size=20
 cd ..
 make
 printf "Scull buffer device at: /dev/scullBuffer0\n"
